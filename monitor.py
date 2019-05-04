a86d7bbf304ab6221cba3574e37b01c8188106ef
pangduckwai <paul.lai@sea9.org>
2019-05-05 04:18:46 +0800

    Add remount function

--- monitor.py
+++ monitor.py
@@ -35,10 +35,13 @@ CHAR_WIDTH = 19
 font = ImageFont.load_default()
 width = 128
 height = 64
-padding = -2
-top = padding
-bottom = height-padding
-xpos = 0
+x0 = 0
+x1 = 70
+y0 = -2
+y1 = 12
+# ypos2 = 24
+# ypos3 = 36
+# ypos4 = 48
 
 # init GPIO
 GPIO.setmode(GPIO.BCM) 
@@ -58,87 +61,123 @@ draw = ImageDraw.Draw(Image.new('1', (width, height)))
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
 state = 0 #System state: 0 - scrren is off; equal to channel number (e.g. BTN2_PIN, JS_P_PIN) otherwise
-stops = 0 #Shutdown choice: 0 - No; 1 - Yes
+horz = 0 #Selection choice: 0 - Left; 1 - Right
+vert = 4 #Selection choice: 1 - Top; 2 - Second; 3 - Third; 4 - Bottom
 stamp = time.time() #Current timestamp
 start = time.time() #Start screen saver count down
 
-def shutdown(channel):
-	global stops
+def take_act(channel):
+	global start
+	global vert
 	if state == BTN3_PIN:
-		if stops == 1:
-			draw_process(RST_PIN)
+		if vert == 1:
+			draw_scn(RST_PIN)
 			os.system("sudo shutdown -h now")
+		elif vert == 2:
+			# Remount
+			draw_scn(997)
+			start = stamp - SCREEN_SAVER - 10
+			os.system("sudo /usr/bin/usb_drive.sh remount")
+		elif vert == 3:
+			# Replug USB
+			draw_scn(998)
+			start = stamp - SCREEN_SAVER - 10
+			os.system("sudo /usr/bin/usb_drive.sh replug")
 		else:
-			main_process(BTN1_PIN)
+			show_stt(BTN1_PIN)
 	else:
-		stops = 0
-		main_process(BTN3_PIN)
+		vert = 4 # Default
+		show_stt(BTN3_PIN)
 
-def select(channel):
-	global stops
-	if state == BTN3_PIN:
-		if channel == JS_L_PIN:
-			stops = 1
-			draw_process(state)
-		elif channel == JS_R_PIN:
-			stops = 0
-			draw_process(state)
-		else:
-			pass
+def select_h(channel):
+	global start
+	global horz
+	if channel == JS_L_PIN:
+		start = time.time()
+		horz = 1
+		draw_scn(state)
+	elif channel == JS_R_PIN:
+		start = time.time()
+		horz = 0
+		draw_scn(state)
+	else:
+		pass
 
-def draw_process(channel):
+def select_v(channel):
+	global start
+	global vert
+	if channel == JS_U_PIN:
+		start = time.time()
+		if vert > 1:
+			vert = vert - 1
+	elif channel == JS_D_PIN:
+		start = time.time()
+		if vert < 4:
+			vert = vert + 1
+	else:
+		pass
+
+def draw_scn(channel):
 	with canvas(device) as draw:
 		LINE0 = subprocess.check_output("date +\"%Y-%m-%d %H:%M:%S\"", shell = True)
 		LINE1 = ""
 		LINE2 = ""
 		LINE3 = ""
 		LINE4 = ""
-		if channel == BTN2_PIN:
-			LINE1 = subprocess.check_output("hostname -I | awk '{printf \"IP :%s\", $1}'", shell = True )
-			LINE2 = subprocess.check_output("top -bn1 | awk 'NR==3{printf \"CPU:%.1f%% idle\", $8}'", shell = True )
-			LINE3 = subprocess.check_output("free -mh | awk 'NR==2{printf \"Mem:%s/%s %.1f%%\", $3,$2,$3*100/$2 }'", shell = True )
-			LINE4 = subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp | awk '{printf \"Tmp:%.1fC\", $1/1000}'", shell = True )
-		elif channel == BTN1_PIN:
-			ssid = subprocess.check_output("iwgetid --raw | awk '{printf \"WiFi:%s\", $0}'", shell = True)
-			freq = subprocess.check_output("iwgetid --freq | awk '{gsub(/Frequency:/,\"\"); printf \" %.1f %s\", $2,$3}'", shell = True)
-			LINE1 = subprocess.check_output("df -h /mnt/usb | awk '$NF==\"/mnt/usb\"{printf \"Disk:%s/%s %s\", $3,$2,$5}'", shell = True )
-			LINE2 = ssid + freq
-			LINE3 = "  in Kbps  out Kbps"
-			LINE4 = subprocess.check_output("ifstat -bT 0.1 1 | awk 'NR==3{printf \"%9.2f %9.2f\",$3,$4}'", shell = True)
-		elif channel == BTN3_PIN:
-			LINE1 = "    Shutdown ?"
-			LINE3 = "   Yes       No"
-			if stops == 0:
-				draw.rectangle((72,50,94,52), outline=255, fill=1)
+		if channel == BTN1_PIN:
+			if horz == 0:
+				ssid = subprocess.check_output("iwgetid --raw | awk '{printf \"WiFi:%s\", $0}'", shell = True)
+				freq = subprocess.check_output("iwgetid --freq | awk '{gsub(/Frequency:/,\"\"); printf \" %.1f %s\", $2,$3}'", shell = True)
+				LINE1 = subprocess.check_output("df -h /mnt/usb | awk '$NF==\"/mnt/usb\"{printf \"Disk:%s/%s %s\", $3,$2,$5}'", shell = True )
+				LINE2 = ssid + freq
+				LINE3 = "  in Kbps  out Kbps"
+				LINE4 = subprocess.check_output("ifstat -bT 0.1 1 | awk 'NR==3{printf \"%9.2f %9.2f\",$3,$4}'", shell = True)
+				draw.rectangle((19,61,48,63), outline=255, fill=0)
+				draw.rectangle((49,61,109,63), outline=255, fill=1)
 			else:
-				draw.rectangle((15,50,37,52), outline=255, fill=1)
+				LINE1 = subprocess.check_output("hostname -I | awk '{printf \"IP :%s\", $1}'", shell = True )
+				LINE2 = subprocess.check_output("top -bn1 | awk 'NR==3{printf \"CPU:%.1f%% idle\", $8}'", shell = True )
+				LINE3 = subprocess.check_output("free -mh | awk 'NR==2{printf \"Mem:%s/%s %.1f%%\", $3,$2,$3*100/$2 }'", shell = True )
+				LINE4 = subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp | awk '{printf \"Tmp:%.1fC\", $1/1000}'", shell = True )
+				draw.rectangle((19,61,78,63), outline=255, fill=1)
+				draw.rectangle((79,61,109,63), outline=255, fill=0)
+		elif channel == BTN3_PIN:
+			LINE1 = "> Shutdown"
+			LINE2 = "> Reset SMB"
+			LINE3 = "> Reset USB"
+			LINE4 = "> Cancel"
+			draw.polygon([(x1,y1*vert+6),(x1+7,y1*vert-1),(x1+7,y1*vert+4),(x1+14,y1*vert+4),(x1+14,y1*vert+8),(x1+7,y1*vert+8),(x1+7,y1*vert+13)], outline=255, fill=1)
 		elif channel == RST_PIN:
 			LINE2 = " Shutting down..."
+		elif channel == 997:
+			LINE2 = " Resetting SMB..."
+		elif channel == 998:
+			LINE2 = " Resetting USB..."
 		else:
 			pass
 
-		draw.text((xpos, top),    LINE0, font=font, fill=255)
+		draw.text((x0, y0),    LINE0, font=font, fill=255)
 		if len(LINE1) > CHAR_WIDTH:
-			draw.text((xpos, top+12), LINE1[:CHAR_WIDTH], font=font, fill=255)
+			draw.text((x0, y1), LINE1[:CHAR_WIDTH], font=font, fill=255)
 		else:
-			draw.text((xpos, top+12), LINE1, font=font, fill=255)
+			draw.text((x0, y1), LINE1, font=font, fill=255)
 
 		if len(LINE2) > CHAR_WIDTH:
-			draw.text((xpos, top+28), LINE2[:CHAR_WIDTH], font=font, fill=255)
+			draw.text((x0, y1*2), LINE2[:CHAR_WIDTH], font=font, fill=255)
 		else:
-			draw.text((xpos, top+28), LINE2, font=font, fill=255)
+			draw.text((x0, y1*2), LINE2, font=font, fill=255)
 
 		if len(LINE3) > CHAR_WIDTH:
-			draw.text((xpos, top+40), LINE3[:CHAR_WIDTH], font=font, fill=255)
+			draw.text((x0, y1*3), LINE3[:CHAR_WIDTH], font=font, fill=255)
 		else:
-			draw.text((xpos, top+40), LINE3, font=font, fill=255)
+			draw.text((x0, y1*3), LINE3, font=font, fill=255)
 
 		if len(LINE4) > CHAR_WIDTH:
-			draw.text((xpos, top+52), LINE4[:CHAR_WIDTH], font=font, fill=255)
+			draw.text((x0, y1*4), LINE4[:CHAR_WIDTH], font=font, fill=255)
 		else:
-			draw.text((xpos, top+52), LINE4, font=font, fill=255)
+			draw.text((x0, y1*4), LINE4, font=font, fill=255)
 
-def main_process(channel):
+def show_stt(channel):
 	global serial
 	global device
 	global draw
@@ -155,7 +194,7 @@ def main_process(channel):
 
 			state = channel
 			start = time.time()
-			draw_process(channel)
+			draw_scn(channel)
 		else:
 			pass
 	else: # Display is on
@@ -165,22 +204,23 @@ def main_process(channel):
 		elif (channel > 0) and (channel != state):
 			state = channel
 			start = time.time()
-			draw_process(channel)
+			draw_scn(channel)
 		else: # state > 0 && channel == 0 && not-yet-timeout, refresh screen
-			draw_process(state)
+			draw_scn(state)
 
-GPIO.add_event_detect(BTN1_PIN, GPIO.RISING, callback=main_process, bouncetime=200)
-GPIO.add_event_detect(BTN2_PIN, GPIO.RISING, callback=main_process, bouncetime=200)
-GPIO.add_event_detect(BTN3_PIN, GPIO.RISING, callback=shutdown, bouncetime=200)
-GPIO.add_event_detect(JS_L_PIN, GPIO.RISING, callback=select, bouncetime=200)
-GPIO.add_event_detect(JS_R_PIN, GPIO.RISING, callback=select, bouncetime=200)
+GPIO.add_event_detect(BTN1_PIN, GPIO.RISING, callback=show_stt, bouncetime=200)
+GPIO.add_event_detect(BTN3_PIN, GPIO.RISING, callback=take_act, bouncetime=200)
+GPIO.add_event_detect(JS_L_PIN, GPIO.RISING, callback=select_h, bouncetime=200)
+GPIO.add_event_detect(JS_R_PIN, GPIO.RISING, callback=select_h, bouncetime=200)
+GPIO.add_event_detect(JS_U_PIN, GPIO.RISING, callback=select_v, bouncetime=200)
+GPIO.add_event_detect(JS_D_PIN, GPIO.RISING, callback=select_v, bouncetime=200)
 
-
+# Main Loop
 try:
-	main_process(BTN1_PIN)
+	show_stt(BTN1_PIN)
 	while True:
 		stamp = time.time()
-		main_process(0)
+		show_stt(0)
 		time.sleep(1)
 
 except:
